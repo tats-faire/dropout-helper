ebaddf3f0db35c7ff455daceb5f4e3e2de0f611e
Kurt Thiemann <info@kurtthiemann.com>
2024-04-28 17:38:43 +0200

    add popup that checks and requests required host permissions

---
+++ entry/popup.js
@@ -0,0 +1,46 @@
+
+let env;
+if (typeof browser !== 'undefined') {
+    env = browser;
+} else if (typeof chrome !== 'undefined') {
+    env = chrome;
+} else {
+    env = null;
+}
+
+const statusOk = document.querySelector('.status-ok');
+const statusError = document.querySelector('.status-error');
+const statusMissingPermission = document.querySelector('.status-missing-permission');
+
+statusMissingPermission.addEventListener('click', requestPermissions);
+
+const requiredPermissions = {origins: ["https://www.dropout.tv/*"]};
+
+async function update () {
+    if (env === null) {
+        statusOk.style.display = 'none';
+        statusError.style.display = 'block';
+        statusMissingPermission.style.display = 'none';
+        return;
+    }
+
+    if (await chrome.permissions.contains(requiredPermissions)) {
+        statusOk.style.display = 'block';
+        statusError.style.display = 'none';
+        statusMissingPermission.style.display = 'none';
+        return;
+    }
+
+    statusOk.style.display = 'none';
+    statusError.style.display = 'none';
+    statusMissingPermission.style.display = 'block';
+}
+
+async function requestPermissions () {
+    await env.permissions.request(requiredPermissions);
+    await update();
+}
+
+(async () => {
+    await update();
+})();
